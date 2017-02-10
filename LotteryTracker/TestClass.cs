692888fe16461d10f0de17f976dca1b7f23a4a84
juanmafc <juanmafc25@gmail.com>
2017-02-10 15:15:09 -0300

    Dummy test project, method and class added

---
+++ LotteryTracker/TestClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryTracker
+{
+    public class TestClass {
+
+        public int dameUn5() {
+            return 5;
+        }
+
+    }
+}
---
+++ LotteryTrackerTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LotteryTracker;
+
+namespace LotteryTrackerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1() {
+            TestClass prueba = new TestClass();
+            Assert.AreEqual(5, prueba.dameUn5() );
+        }
+    }
+}
