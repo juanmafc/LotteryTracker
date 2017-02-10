673786f3f8b25d936ed1816cffe144214738697b
juanmafc <juanmafc25@gmail.com>
2017-02-10 16:32:49 -0300

    Refactor: getWinningNumber moved to QuinielaLottery

--- LotteryTracker/Program.cs
+++ LotteryTracker/Program.cs
@@ -12,70 +12,9 @@ using HtmlAgilityPack;
 namespace LotteryTracker
 {
     class HttpWebRequest_Connection
-    {
-
-        static int getWinnerNumberForDate(string date) {
-            try
-            {                
-
-                //Get winner numbers
-                HttpWebRequest myHttpWebRequest1 = (HttpWebRequest) WebRequest.Create("http://www.loteria-nacional.gob.ar/Internet/Extractos/resultados_i.php?juego=quiniela&fechasorteo=09022017&tiposorteo=pri");
-                myHttpWebRequest1.KeepAlive = false;
-                HttpWebResponse myHttpWebResponse1 = (HttpWebResponse)myHttpWebRequest1.GetResponse();                
-                HtmlDocument cuerpo = new HtmlDocument();                
-                cuerpo.Load( myHttpWebResponse1.GetResponseStream() );
-
-
-                //Get Winner Number
-                HtmlNode primeraColumna = cuerpo.GetElementbyId("Columna1Quiniela");
-                List<HtmlNode> listaNumeros = primeraColumna.ChildNodes.Where(nodo => (nodo.Attributes["class"] != null && nodo.Attributes["class"].Value.Equals("BolillaVertical"))).ToList();                
-                HtmlNode primerNumero = listaNumeros.ElementAt(0);                
-                int x = Int32.Parse(primerNumero.InnerText);
-                Console.WriteLine("Numero ganador: " + x );
-                Console.ReadLine();
-                                                
-                myHttpWebResponse1.Close();
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine("Press ArgumentException Enter Key to Continue..........");
-                Console.ReadLine();
-
-                Console.WriteLine("\nThe second HttpWebRequest object has raised an Argument Exception as 'Connection' Property is set to 'Close'");
-                Console.WriteLine("\n{0}", e.Message);
-
-
-            }
-            catch (WebException e)
-            {
-
-                Console.WriteLine("Press WebException Enter Key to Continue..........");
-                Console.ReadLine();
-
-                Console.WriteLine("WebException raised!");
-                Console.WriteLine("\n{0}", e.Message);
-                Console.WriteLine("\n{0}", e.Status);
-            }
-            catch (Exception e)
-            {
-
-                Console.WriteLine("Press Exception Enter Key to Continue..........");
-
-
-                Console.WriteLine("Exception raised!");
-                Console.WriteLine("Source :{0} ", e.Source);
-                Console.WriteLine("Message :{0} ", e.Message);
-
-                Console.ReadLine();
-            }
-
-
-            return 1;
-        }
-
-
+    {        
         static void Main() {
-            getWinnerNumberForDate("09022017");
+            
         }
     }
 
--- LotteryTracker/QuinielaLottery.cs
+++ LotteryTracker/QuinielaLottery.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +12,65 @@ namespace LotteryTracker
     {
         public override int getFirstNumberOn(int day, int month, int year)
         {
-            return 2000;
+
+            int firstNumber = -1;
+
+            try
+            {
+
+                //Get winner numbers
+                HttpWebRequest myHttpWebRequest1 = (HttpWebRequest)WebRequest.Create("http://www.loteria-nacional.gob.ar/Internet/Extractos/resultados_i.php?juego=quiniela&fechasorteo=10022017&tiposorteo=mat");
+                myHttpWebRequest1.KeepAlive = false;
+                HttpWebResponse myHttpWebResponse1 = (HttpWebResponse)myHttpWebRequest1.GetResponse();
+                HtmlDocument cuerpo = new HtmlDocument();
+                cuerpo.Load(myHttpWebResponse1.GetResponseStream());
+
+
+                //Get Winner Number
+                HtmlNode primeraColumna = cuerpo.GetElementbyId("Columna1Quiniela");
+                List<HtmlNode> listaNumeros = primeraColumna.ChildNodes.Where(nodo => (nodo.Attributes["class"] != null && nodo.Attributes["class"].Value.Equals("BolillaVertical"))).ToList();
+                HtmlNode primerNumero = listaNumeros.ElementAt(0);
+                firstNumber = Int32.Parse(primerNumero.InnerText);
+                Console.WriteLine("Numero ganador: " + firstNumber);
+                Console.ReadLine();
+
+                myHttpWebResponse1.Close();
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine("Press ArgumentException Enter Key to Continue..........");
+                Console.ReadLine();
+
+                Console.WriteLine("\nThe second HttpWebRequest object has raised an Argument Exception as 'Connection' Property is set to 'Close'");
+                Console.WriteLine("\n{0}", e.Message);
+
+
+            }
+            catch (WebException e)
+            {
+
+                Console.WriteLine("Press WebException Enter Key to Continue..........");
+                Console.ReadLine();
+
+                Console.WriteLine("WebException raised!");
+                Console.WriteLine("\n{0}", e.Message);
+                Console.WriteLine("\n{0}", e.Status);
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine("Press Exception Enter Key to Continue..........");
+
+
+                Console.WriteLine("Exception raised!");
+                Console.WriteLine("Source :{0} ", e.Source);
+                Console.WriteLine("Message :{0} ", e.Message);
+
+                Console.ReadLine();
+            }
+
+            return firstNumber;
+            
         }
     }
 }
