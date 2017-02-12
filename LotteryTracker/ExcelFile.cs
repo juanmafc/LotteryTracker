7f9b8514673fd2fe799c247f8c00031ad411dcf4
juanmafc <juanmafc25@gmail.com>
2017-02-12 18:00:39 -0300

    ExcelFile and Excel test class added
    TestFile added

---
+++ LotteryTracker/ExcelFile.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+using Excel = Microsoft.Office.Interop.Excel;
+
+
+namespace LotteryTracker
+{
+    /* TODO: extract this as a class
+    public class ExcelApplication
+    {
+        static private Excel.Application application;
+
+        private ExcelApplication() { }
+                 
+        static public ExcelFile openExcelFile(string path)
+        {
+
+            return new ExcelFile(path);
+        }
+
+    }
+    */
+
+        public class ExcelFile
+    {
+
+        private Excel.Application application;
+
+        private Excel.Workbook file;
+
+        private string path;
+       
+
+        public ExcelFile(string path)
+        {
+            //TODO: release resources and extract this to a singleton class
+            this.application = new Excel.Application();
+            this.application.Visible = false;
+            if (this.application == null)
+            {
+                Console.WriteLine("Excel is not properly installed!!");
+                return;
+            }
+            //////////////////////////////////////////////////////////////
+
+
+            this.file= null;
+            this.path = System.IO.Path.GetFullPath(path);
+            this.file = this.application.Workbooks.Open(this.path);
+            /*
+            Excel.Worksheet MySheet = null;
+            MySheet = MyBook.Sheets[1];
+            MySheet.Range["A1:J10"].Interior.Color = "0xFF0000";            
+            */
+                        
+        }
+
+        public void close()
+        {
+            //TODO: rethink this method
+            //this.file.Close(false);
+            //Marshal.ReleaseComObject(this.file);
+        }
+
+        public void save()
+        {
+            this.file.Save();
+        }
+
+        public ExcelSheet getSheet(int sheetNumber)
+        {
+            return new ExcelSheet(this.file.Sheets[sheetNumber]);
+        }
+
+        ~ExcelFile()
+        {
+            this.file.Close(false);
+            this.application.Quit();
+
+            Marshal.ReleaseComObject(this.file);
+            Marshal.ReleaseComObject(this.application);
+        }
+
+    }
+
+    public class ExcelSheet
+    {
+        private Excel.Worksheet sheet;
+
+        public ExcelSheet(Excel.Worksheet sheet)
+        {
+            this.sheet = sheet;
+        }
+
+        ~ExcelSheet()
+        {
+            Marshal.ReleaseComObject(this.sheet);
+        }
+
+        public string getCellText(int row, int column)
+        {
+            return this.sheet.Cells[row, column].Value.ToString();
+        }
+
+        public void setCellText(int row, int column, string text)
+        {
+            this.sheet.Cells[row, column] = text;
+        }
+
+        //        int lastRow = MySheet.Cells.SpecialCells(Excel.XlCellType.xlCellTypeLastCell).Row;
+        //        Console.WriteLine(lastRow);
+        //        lastRow += 1;
+        //        MySheet.Range["A1:J10"].Interior.Color = Excel.XlRgbColor.rgbRed;
+        //        //Para sacar el valor de la Celda
+        //        Console.WriteLine( MySheet.Cells[150, 1].Value.ToString() );
+        //        int cantidadDeDias = Int32.Parse(MySheet.Cells[1, 1].Value.ToString());
+        //        cantidadDeDias++;
+        //        Console.WriteLine( cantidadDeDias );
+
+        //        MyBook.Save();
+        //        MyBook.Close();
+
+    }
+}
--- LotteryTrackerTests/UnitTest1.cs
+++ LotteryTrackerTests/UnitTest1.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using LotteryTracker;
+using System.IO;
+using System.Text;
 
 namespace LotteryTrackerTests
 {
@@ -24,17 +26,86 @@ namespace LotteryTrackerTests
 
     }
 
-    /*
+    
     [TestClass]
     public class ExcelTests
     {
+
+        //Setup empty test file
+        [TestInitialize]
+        public void setUp()
+        {
+            //TODO: create empty file and start Excel
+        }
+
+        [TestCleanup]
+        public void tearDown()
+        {
+            //TODO: delete empty file and quit Excel
+        }
+        
+
+        //cleanup delete empty test file
         [TestMethod]
         public void TestOpenEditA1CellAndClose()
         {
-            ExcelFile file = new ExcelFile("TestFile.xlsx");
-            file.delete();            
-        }
+            //Excel is closed and then opened, refactor this
+            {
+                ExcelFile file = new ExcelFile(@"..\..\TestFiles\TestFile.xlsx");
+                ExcelSheet sheet = file.getSheet(1);
+                sheet.setCellText(1, 1, "PRUEBA");
+                file.save();
+            }
 
+            {
+                ExcelFile secondFile = new ExcelFile(@"..\..\TestFiles\TestFile.xlsx");
+                ExcelSheet resultSheet = secondFile.getSheet(1);
+                resultSheet.setCellText(1, 1, "PRUEBA");
+                Assert.AreEqual("PRUEBA", resultSheet.getCellText(1, 1));
+            }            
+
+
+            //PROBLEMA: SI HAGO EL CLOSE, DESPUES EXPLOTA EN EL DESTRUCTOR
+            //file.close();
+            //file.delete();
+
+            /*
+            string fileName = @"LCDTMAB.txt";
+
+            try
+            {
+                // Check if file already exists. If yes, delete it. 
+                if (File.Exists(fileName))
+                {
+                    File.Delete(fileName);
+                }
+
+                // Create a new file 
+                using (FileStream fs = File.Create(fileName))
+                {
+                    // Add some text to file
+                    Byte[] title = new UTF8Encoding(true).GetBytes("New Text File");
+                    fs.Write(title, 0, title.Length);
+                    byte[] author = new UTF8Encoding(true).GetBytes("Mahesh Chand");
+                    fs.Write(author, 0, author.Length);
+                }
+
+                // Open the stream and read it back.
+                using (StreamReader sr = File.OpenText(fileName))
+                {
+                    string s = "";
+                    while ((s = sr.ReadLine()) != null)
+                    {
+                        Console.WriteLine(s);
+                    }
+                }
+            }
+            catch (Exception Ex)
+            {
+                Console.WriteLine(Ex.ToString());
+            }
+            */
+        }
     }
-    */
+    
 }
