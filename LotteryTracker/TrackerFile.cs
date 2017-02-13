15176a3886753d1cb792c8244113f719cd2e026e
juanmafc <juanmafc25@gmail.com>
2017-02-13 10:39:44 -0300

    TrackerFiler class and test added

---
+++ LotteryTracker/TrackerFile.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LotteryTracker
+{
+    public class TrackerFile
+    {
+        private readonly int COLUMNS_COUNT = 13;
+        private ExcelFile file;
+
+        private SortedDictionary<int, string> monthsMap;
+
+        public TrackerFile(string path)
+        {
+            this.file = new ExcelFile(path);
+            this.monthsMap = new SortedDictionary<int, string>();
+            this.monthsMap[2] = "Febrero";
+        }
+
+        ~TrackerFile()
+        {
+            //TODO: For some reason saving in destructor doesn't work
+            //this.file.save();
+        }
+
+        public void addEmptyEntry(int day, int month, int year)
+        {
+            ExcelSheet sheet = this.file.getSheet(3);
+            int newRow = sheet.getLastUsedRowNumber() + 1;
+
+            sheet.setCellText(newRow, 1, day.ToString() );
+            sheet.setCellText(newRow, 2, this.getMonthAsString(month) );
+            sheet.setCellText(newRow, 3, "" );
+
+            for (int i = 1; i <= this.COLUMNS_COUNT; i++)
+            {
+                sheet.setCellColor(newRow, i, 148, 138, 84);
+            }
+            this.file.save();
+        }
+
+        private string getMonthAsString(int month)
+        {
+            return this.monthsMap[month];
+        }
+    }
+}
---
+++ LotteryTrackerTests/TrackerFIleTests.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LotteryTracker;
+
+namespace LotteryTrackerTests
+{    
+    [TestClass]
+    public class TrackerFIleTests
+    {        
+
+        [TestMethod]
+        public void TestTrackerFileAddsAnEmptyEntryOnFebruary12th2017()
+        {
+            TrackerFile file = new TrackerFile(@"..\..\TestFiles\LotteryTrackerFile.xlsx");
+            file.addEmptyEntry(12, 2, 2017);
+            //TODO: automate this assert
+        }
+    }
+}
