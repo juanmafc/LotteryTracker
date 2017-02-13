818daf056dd512053db2be55979ce890502c50ef
juanmafc <juanmafc25@gmail.com>
2017-02-13 08:18:40 -0300

    Tests classes extracted to independent files

---
+++ LotteryTrackerTests/LotteryTests.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LotteryTracker;
+using System.IO;
+using System.Text;
+
+
+namespace LotteryTrackerTests
+{
+    [TestClass]
+    public class LotteryTests
+    {
+        [TestMethod]
+        public void TestGetWinnerNumberOn10thFebruary2017()
+        {
+            Lottery nationalLottery = new QuinielaLottery();
+            //8020 is a known number
+            Assert.AreEqual("8020", nationalLottery.getFirstNumberOn("10022017"));
+        }
+
+        [TestMethod]
+        public void TestGetWinnerNumberOn9thFebruary2017()
+        {
+            Lottery nationalLottery = new QuinielaLottery();
+            //5948 is a known number
+            Assert.AreEqual("5948", nationalLottery.getFirstNumberOn("09022017"));
+        }
+
+        [TestMethod]
+        public void TestGetOnlyWinnerNumbersLastDigitOn9thFebruary2017()
+        {
+            Lottery nationalLottery = new QuinielaLottery();
+            //5948 is a known number
+            Assert.AreEqual("8", nationalLottery.getFirstNumberOn("09022017", 1));
+        }
+
+    }    
+}
---
+++ LotteryTrackerTests/StringDateTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LotteryTracker;
+
+namespace LotteryTrackerTests
+{
+    [TestClass]
+    public class StringDateTests
+    {
+
+        [TestMethod]
+        public void TestCreateAStringDateAndGetItInDDMMYYYYformat()
+        {
+            StringDate date = new StringDate(1, 2, 2017);
+            Assert.AreEqual("01022017", date.getDate());
+        }
+
+        [TestMethod]
+        public void TestIncrementDateByOneDay()
+        {
+            StringDate date = new StringDate(1, 1, 2017);
+            date.nextDay();
+            Assert.AreEqual("02012017", date.getDate());
+        }
+
+        [TestMethod]
+        public void TestIncrementDateByOneDayAndThatIncrementChangesTheMonth()
+        {
+            StringDate date = new StringDate(31, 1, 2017);
+            date.nextDay();
+            Assert.AreEqual("01022017", date.getDate());
+        }
+
+        [TestMethod]
+        public void TestIncrementDateByOneDayAndThatIncrementChangesTheYear()
+        {
+            StringDate date = new StringDate(31, 12, 2015);
+            date.nextDay();
+            Assert.AreEqual("01012016", date.getDate());
+        }
+
+        [TestMethod]
+        public void TestCheckIfIsSundayReturnsTrueOnASundayDate()
+        {
+            //12nd February 2017 was on Sunday
+            StringDate date = new StringDate(12, 2, 2017);
+            Assert.IsTrue(date.isSunday());
+        }
+
+        [TestMethod]
+        public void TestCheckIfIsSundayReturnsFalseOnANonSundayDate()
+        {
+            //3rd February 2017 was on Friday
+            StringDate date = new StringDate(3, 2, 2017);
+            Assert.IsFalse(date.isSunday());
+        }
+    }
+}
