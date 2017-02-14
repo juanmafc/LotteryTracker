d2de689c6f539ca315e5300bd289b9a8021b4e28
juanmafc <juanmafc25@gmail.com>
2017-02-14 10:44:18 -0300

    TrackerFile working as intended (colloring missing)
    Refactoring needed, rushed code

--- LotteryTracker/TrackerFile.cs
+++ LotteryTracker/TrackerFile.cs
@@ -8,23 +8,114 @@ namespace LotteryTracker
 {
     public class TrackerFile
     {
-        private readonly int COLUMNS_COUNT = 13;
+        private readonly int COLUMNS_COUNT = 13;        
         private ExcelFile file;
 
         private SortedDictionary<int, string> monthsMap;
+        private SortedDictionary<int, string> columnsToNumbersMap;
 
         public TrackerFile(string path)
         {
             this.file = new ExcelFile(path);
             this.monthsMap = new SortedDictionary<int, string>();
+            this.monthsMap[1] = "Enero";
             this.monthsMap[2] = "Febrero";
+            this.monthsMap[3] = "Marzo";
+            this.monthsMap[4] = "Abril";
+            this.monthsMap[5] = "Mayo";
+            this.monthsMap[6] = "Junio";
+            this.monthsMap[7] = "Julio";
+            this.monthsMap[8] = "Agosto";
+            this.monthsMap[9] = "Septiembre";
+            this.monthsMap[10] = "Octubre";
+            this.monthsMap[11] = "Noviembre";
+            this.monthsMap[12] = "Diciembre";
+
+            //TODO: refactor and constants
+            this.columnsToNumbersMap = new SortedDictionary<int, string>();
+            this.columnsToNumbersMap[4] = "0";
+            this.columnsToNumbersMap[5] = "1";
+            this.columnsToNumbersMap[6] = "2";
+            this.columnsToNumbersMap[7] = "3";
+            this.columnsToNumbersMap[8] = "4";
+            this.columnsToNumbersMap[9] = "5";
+            this.columnsToNumbersMap[10] = "6";
+            this.columnsToNumbersMap[11] = "7";
+            this.columnsToNumbersMap[12] = "8";
+            this.columnsToNumbersMap[13] = "9";
         }
+
+        public void addEntryForWinningNumber(int day, int month, int year, string winningNumber)
+        {
+            ExcelSheet sheet = this.file.getSheet(3);
+            int lastRow = sheet.getLastUsedRowNumber();
+            int newRow =  lastRow + 1;
+
+
+            
+            sheet.setCellText(newRow, 1, day.ToString());
+            sheet.setCellText(newRow, 2, this.getMonthAsString(month));
+            sheet.setCellText(newRow, 3, winningNumber);
+            
+            while ( this.isRowAnEmptyEntry(sheet, lastRow) )
+            {
+                lastRow--;
+            }
+
+            for (int i = 4; i <= 13; i++)
+            {
+                if (this.columnsToNumbersMap[i].Equals(winningNumber))
+                {
+                    sheet.setCellText(newRow, i, "SALIO");
+                    sheet.setCellColor(newRow, i, 0, 255, 255);
+                }
+                else
+                {
+                    this.addNonWinningEntry(lastRow, i, newRow, sheet);                    
+                }
+            }
+            this.file.save();
+
+        }
+
+        private void addNonWinningEntry(int lastRow, int column, int newRow, ExcelSheet sheet )
+        {
+            string days = sheet.getCellText(lastRow, column);
+
+            if (days.Equals("SALIO"))
+            {
+                sheet.setCellText(newRow, column, "1 DIA");
+            }
+            else if (days.Equals("1 DIA"))
+            {
+                sheet.setCellText(newRow, column, "2 DIAS");
+            }
+            else
+            {
+                days = days.Replace(" DIAS", "");
+                int numberOfDays = Int32.Parse(days);
+                numberOfDays++;
+
+                days = numberOfDays.ToString() + " DIAS";
+                sheet.setCellText(newRow, column, days);
+            }
+        }
+
+
+
+
+        private bool isRowAnEmptyEntry(ExcelSheet sheet, int row)
+        {
+            return ((sheet.getCellText(row, 3).Equals("")) || (sheet.getCellText(row, 3).Equals("DOMINGO")));
+        }
+
 
         ~TrackerFile()
         {
             //TODO: For some reason saving in destructor doesn't work
             //this.file.save();
         }
+
 
         public void addEmptyEntry(int day, int month, int year)
         {
--- LotteryTrackerTests/TrackerFIleTests.cs
+++ LotteryTrackerTests/TrackerFIleTests.cs
@@ -9,12 +9,27 @@ namespace LotteryTrackerTests
     [TestClass]
     public class TrackerFIleTests
     {        
-
+                
         [TestMethod]
         public void TestTrackerFileAddsAnEmptyEntryOnFebruary12th2017()
         {
             TrackerFile file = new TrackerFile(@"..\..\TestFiles\LotteryTrackerFile.xlsx");
             file.addEmptyEntry(12, 2, 2017);
+            //TODO: automate this assert
+        }
+
+        [TestMethod]
+        public void TestTrackerFileAddAnEntryForAWinningNumber5OnFebruary12th2017()
+        {
+            TrackerFile file = new TrackerFile(@"..\..\TestFiles\LotteryTrackerFile2.xlsx");                       
+            file.addEntryForWinningNumber(12, 2, 2017, "3");
+            
+            for (int i = 0; i < 20; i ++)
+            {
+                Random rd = new Random();
+                file.addEntryForWinningNumber(12, 2, 2017, rd.Next(10).ToString() );
+            }
+            
             //TODO: automate this assert
         }
     }
