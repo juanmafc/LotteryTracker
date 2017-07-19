8e049f023080a7e16fcbe9a39e44bf3f9a7cb6aa
juanmafc <juanmafc25@gmail.com>
2017-07-19 19:14:22 -0300

    Chrome Extension basic structure  added

---
+++ LotteryTrackerChromeExtension/popup.js
@@ -0,0 +1,15 @@
+var referenciaAlBoton = document.getElementById("buttonIrLoteria");
+referenciaAlBoton.addEventListener("click", funcionAlClickearBoton);
+
+
+
+function funcionAlClickearBoton() {
+    console.log("1\n");
+    //https://www.w3schools.com/xml/xml_http.asp
+    //http://stackoverflow.com/questions/247483/http-get-request-in-javascript
+    //var requester = new XMLHttpRequest();
+    //requester.open()
+}
+
+
+
