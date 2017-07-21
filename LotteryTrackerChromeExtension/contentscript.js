c7a75d6d6b0dee4f6422adc94fc3ee5a9f62ff56
juanmafc <juanmafc25@gmail.com>
2017-07-20 22:57:38 -0300

    Previous Day Iteration working but it misses some days

--- LotteryTrackerChromeExtension/contentscript.js
+++ LotteryTrackerChromeExtension/contentscript.js
@@ -1,22 +1,40 @@
-var resultsIframe = $('body>center>table>tbody>tr>td>table>tbody>tr:nth-child(3)>td:nth-child(2)>table>tbody>tr:nth-child(2)>td>table>tbody>tr>td:nth-child(4)>iframe');
 
+var resultsIframe; 
 var i = 0;
-function getPreviousDayResults() {
-    if (i < 5 ){
-        i++;
-        //Save winner numbers
-        resultsIframe.contents().find("form")[0].submit();
+var previousDaysCount = 0;
+
+function levantarTodo() {
+
+    console.log("Se entro a levantar todo");
+    resultsIframe = $('body>center>table>tbody>tr>td>table>tbody>tr:nth-child(3)>td:nth-child(2)>table>tbody>tr:nth-child(2)>td>table>tbody>tr>td:nth-child(4)>iframe');    
+    console.log(resultsIframe);
+    console.log("JQuery murio?");
+
+    function getPreviousDayResults() {
+        if (i < previousDaysCount ){
+            i++;
+            //Save winner numbers
+            resultsIframe.contents().find("form")[0].submit();
+        }
     }
+    console.log("Se definio la funcion");
+    
+    resultsIframe.ready(function() {            
+        console.log("Se entro al ready");
+        resultsIframe.bind("load", getPreviousDayResults);
+        console.log("el bind no explota");
+        //console.log(resultsIframe);
+        //console.log(resultsIframe[0].contentWindow);
+        resultsIframe[0].contentWindow.location.reload(true);
+        console.log("el reload?");
+    });
+
+    console.log("Se cargo el ready");
 }
 
-resultsIframe.ready(function() {            
-    resultsIframe.bind("load", getPreviousDayResults);
-    resultsIframe.contentWindow.location.reload(true);
-});
+function mensajesListener(request, sender, sendResponse) {        
+    previousDaysCount = request.daysCount;    
+    levantarTodo();    
+}
 
-
-chrome.runtime.onMessage.addListener(
-  function(request, sender, sendResponse) {
-    console.log("Mensaje recibido");
-    console.log(request.daysCount);
-  });
+chrome.runtime.onMessage.addListener(mensajesListener);
