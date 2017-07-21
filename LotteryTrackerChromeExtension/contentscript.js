b269ef80c9d2b8bae1e0801f3b61d6fd683cba38
juanmafc <juanmafc25@gmail.com>
2017-07-21 00:23:02 -0300

    Timeout added between submits calls

--- LotteryTrackerChromeExtension/contentscript.js
+++ LotteryTrackerChromeExtension/contentscript.js
@@ -3,27 +3,32 @@ var resultsIframe;
 var i = 0;
 var previousDaysCount = 0;
 
-function levantarTodo() {
-    
-    resultsIframe = $('body>center>table>tbody>tr>td>table>tbody>tr:nth-child(3)>td:nth-child(2)>table>tbody>tr:nth-child(2)>td>table>tbody>tr>td:nth-child(4)>iframe');    
-        
-    function getPreviousDayResults() {
-        if (i < previousDaysCount ){
-            i++;
-            //Save winner numbers
-            resultsIframe.contents().find("form")[0].submit();
-        }
-    }    
-    
+function saveResultsAndGoToPreviousDay() {    
+    //Save winner numbers
+    resultsIframe.contents().find("form")[0].submit();    
+}
+
+function getPreviousDayResults() {
+    //Alternative: while(lastDay != currentDay)
+    if (i < previousDaysCount ){
+        i++;
+        var resultado = 10;
+        setTimeout(saveResultsAndGoToPreviousDay, 150);
+        //NOTE: this line resultsIframe.contents().find("form")[0].submit(); would be executed INMEDIATLY after calling the previous method
+    }
+}
+
+function startPreviousDaysResultsSearch() {
+    resultsIframe = $('body>center>table>tbody>tr>td>table>tbody>tr:nth-child(3)>td:nth-child(2)>table>tbody>tr:nth-child(2)>td>table>tbody>tr>td:nth-child(4)>iframe');                    
     resultsIframe.ready(function() {                    
-        resultsIframe.bind("load", getPreviousDayResults);            
+        resultsIframe.bind("load", getPreviousDayResults);        
         resultsIframe[0].contentWindow.location.reload(true);        
     });    
 }
 
 function mensajesListener(request, sender, sendResponse) {        
-    previousDaysCount = request.daysCount;    
-    levantarTodo();    
+    previousDaysCount = request.daysCount;
+    startPreviousDaysResultsSearch();
 }
 
 chrome.runtime.onMessage.addListener(mensajesListener);
